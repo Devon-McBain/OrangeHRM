3dc9aceb0720e7086c6d0a84183033825c629173
D W MCBAIN - CHARLES <41013743@mylife.unisa.ac.za>
2026-09-23 05:07:47 +0200

    Initial commit

---
+++ tests/ORM_Login.spec.ts
@@ -0,0 +1,5 @@
+import {test, expect} from '@playwright/test';
+
+test('ORM_Login', async ({page}) => {
+
+});
---
+++ tests/test.spec.ts
@@ -0,0 +1,12 @@
+import {test, expect} from '@playwright/test';
+
+test('Go to webpage', async({page}) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await expect(page).toHaveTitle(/OrangeHRM/)
+})
+
+test('Enter login details', async ({page}) => {
+   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+   await page.getByText('username').click();
+   await expect(page.getByText('username')).toBeVisible();
+});
